bfdf7fd1b583232032c8104f240c596a523cd3d6
Burak CAN <drcan94@gmail.com>
2021-12-19 20:49:17 +0300

    Main frontend structure is completed.

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ Navbar -> bg="dark" variant="dark" expand="lg" collapseOnSelect
 
 ##### Fontawesome cdn added to index.html
 <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/> -->
+
+##### Homescreen, product and rating is added
+
+<!-- npm install react-router-dom react-router-bootstrap -->
+<!-- import { BrowserRouter as Router, Route } from 'react-router-dom' -->
+<!-- change the "div" in the App to "Router" -->
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import { Container } from 'react-bootstrap'
-
+import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
     return (
         <div>
-            <Header />
-            <main className='py-3'>
-                <Container>
-                    <h1>
-                        Welcome
-                    </h1>
-                </Container>
-            </main>
-            <Footer />
+        <Header />
+        <main className='py-3'>
+            <Container>
+                <Routes>
+                    <Route path='/' element={<HomeScreen />} exact />
+                    <Route path='/product/:id' element={<ProductScreen />} />
+                </Routes>
+            </Container>
+        </main>
+        <Footer />
         </div>
     );
 }
